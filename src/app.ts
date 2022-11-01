20c9267cadf51bc00cb91b59e274fcea0e5504e1
olusammy <olumorinsammy@gmail.com>
2022-11-01 21:59:05 +0100

    deploy to heroku

--- src/app.ts
+++ src/app.ts
@@ -17,12 +17,19 @@ app.use(cors());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+} else {
+  app.use(express.static(path.join(__dirname, "public")));
+}
 
 app.use("/", indexRouter);
 app.use("/files", fileRouter);
